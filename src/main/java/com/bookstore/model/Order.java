d84f2522fa1d9c0d0636a779b620f77b9ed99f38
MayuranThanushan <mayuranthanushan@gmail.com>
2025-04-16 01:13:12 +0530

    Add core data models and in-memory storage for Bookstore API

--- src/main/java/com/bookstore/model/Order.java
+++ src/main/java/com/bookstore/model/Order.java
@@ -1,4 +1,47 @@
 package com.bookstore.model;
 
+import java.util.List;
+
 public class Order {
+    private int orderId;
+    private int customerId;
+    private List<CartItem> items;
+    private double totalAmount;
+
+    public Order() {}
+
+    public Order(int orderId, int customerId, List<CartItem> items, double totalAmount) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.items = items;
+        this.totalAmount = totalAmount;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
 }
--- src/main/java/com/bookstore/storage/InMemoryDatabase.java
+++ src/main/java/com/bookstore/storage/InMemoryDatabase.java
@@ -1,4 +1,18 @@
 package com.bookstore.storage;
 
+import com.bookstore.model.*;
+
+import java.util.*;
+
 public class InMemoryDatabase {
+    public static final Map<Integer, Book> books = new HashMap<>();
+    public static final Map<Integer, Author> authors = new HashMap<>();
+    public static final Map<Integer, Customer> customers = new HashMap<>();
+    public static final Map<Integer, List<CartItem>> carts = new HashMap<>();
+    public static final Map<Integer, List<Order>> orders = new HashMap<>();
+
+    public static int bookIdCounter = 1;
+    public static int authorIdCounter = 1;
+    public static int customerIdCounter = 1;
+    public static int orderIdCounter = 1;
 }
